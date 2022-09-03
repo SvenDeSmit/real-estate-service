943c43af55e114b98f143751ed084d839f31893f
SvenDeSmit <sven.desmit@telenet.be>
2022-09-03 12:32:30 +0200

    Initial commit

---
+++ src/main/java/be/snife/ibot/realestate/RealEstateServiceApplication.java
@@ -0,0 +1,13 @@
+package be.snife.ibot.realestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateServiceApplication.class, args);
+	}
+
+}
