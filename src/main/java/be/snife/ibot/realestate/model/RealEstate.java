75ce78d18444402653025b55e7f827f7f6c2627f
SvenDeSmit <sven.desmit@telenet.be>
2022-09-05 13:54:19 +0200

    First domain model

--- src/main/java/be/snife/ibot/realestate/model/RealEstate.java
+++ src/main/java/be/snife/ibot/realestate/model/RealEstate.java
@@ -1,14 +1,28 @@
 package be.snife.ibot.realestate.model;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@ToString
+@Slf4j
 public class RealEstate extends Composite {
 
+	public String toString() {
+		ObjectMapper om = new ObjectMapper();
+		String res = null;
+		try {
+			res = om.writeValueAsString(this);
+		} catch(JsonProcessingException e) {
+			log.error("Cannot create JSON from RealEstate object",e);
+		}
+		return res;
+	}
 }
--- src/test/java/be/snife/ibot/realestate/model/RealEstateTest.java
+++ src/test/java/be/snife/ibot/realestate/model/RealEstateTest.java
@@ -14,34 +14,8 @@ class RealEstateTest {
 
 	@Test
 	void test() {
-		RealEstate re = new RealEstate();
-
-		CadasterAddress addr = new CadasterAddress();
-		addr.setZipCode("9160");
-		addr.setCity("Lokeren");
-		addr.setStreet("Lokerenstraat");
-		addr.setNumber("111");
-		addr.setNisCode("my-nis-code");
-		
-		PlanParcel plp = PlanParcel.builder()
-				.capakey(new Capakey("11111", "A", "2222", "33", "B", "444",null))
-				.build();
-		
-		PatrimoniumParcel pap = PatrimoniumParcel.builder()
-				.creationDate(new GregorianCalendar(2010,9,5).getTime())
-				.natureCode("nat-code-1").natureDescription("Nature description")
-				.statuteCode("stat-code-1").statuteDescription("Statute description")
-				.utilizationCode("util-code-1").utilizationDescription("Utilization description")
-				.partNumber("P0001")
-				.planParcel(plp)
-				.build();
-		pap.setAddress(addr);
-		
-		re.getComposition().add(pap);
-		
-		log.debug(re.toString());
-		
-		
+		RealEstate re = RealEstateModelHelper.getRealEState1();
+		log.debug(re.toString());		
 	}
 
 }
